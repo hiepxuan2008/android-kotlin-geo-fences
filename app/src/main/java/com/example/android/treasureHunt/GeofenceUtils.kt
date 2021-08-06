5523493b30883777dee28b24d03753596e21ab1d
hiepxuan2008 <hiepxuan2008@gmail.com>
2021-08-06 19:34:32 +0700

    Completed code

--- app/src/main/java/com/example/android/treasureHunt/GeofenceUtils.kt
+++ app/src/main/java/com/example/android/treasureHunt/GeofenceUtils.kt
@@ -58,7 +58,7 @@ internal object GeofencingConstants {
             "golden_gate_bridge",
             R.string.golden_gate_bridge_hint,
             R.string.golden_gate_bridge_location,
-            LatLng(37.819927, -122.478256)),
+            LatLng(37.81056081327915, -122.47692623486246)),
 
         LandmarkDataObject(
             "ferry_building",
